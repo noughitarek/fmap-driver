89daef28fcfcce20d6ca67e70e28d58115f32834
Tarek <noughitarek@gmail.com>
2024-09-08 12:41:38 +0200

    updating remote url

--- bot.py
+++ bot.py
@@ -2,5 +2,5 @@ from driver import Driver
 # 
 
 if __name__ == "__main__":
-    driver = Driver('http://127.0.0.1:8000/api/v1')
+    driver = Driver('http://fmap.ecoshark.org/api/v1')
     driver.start()
